bf1e77ccf8eb9b19cbd1b754ec75db1cfb64d28a
lucabaiocchi <73413708+lucabaiocchi@users.noreply.github.com>
2022-09-20 00:01:39 -0400

    Update ScoreManager.cs

--- Assets/Scripts/UI/ScoreManager.cs
+++ Assets/Scripts/UI/ScoreManager.cs
@@ -8,17 +8,32 @@ public class ScoreManager : MonoBehaviour
     public int playerScore = 0;
     public Text scoreText;
     public UpdateScore updater;
-
-    // Update is called once per frame
+    public bool gameEnded;
+    public Text gameHasEnded;
+    //public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameHasEnded = GameObject.Find("gameHasEnded").GetComponent<Text>();
+        gameHasEnded.enabled = false;
+        playerScore = 0;
+    }
     private void Update()
     {
-        if (playerScore > 1999) { 
+        if (playerScore > 200) {
+            gameEnded = true;
+            gameHasEnded.enabled = true;
+            //TimerOn = false;
+            gameHasEnded.text = "You Won!!";
+            Time.timeScale = 0;
+
         }
     }
-
-    public void UpdateScore(int scoreObtained)
+    // Update is called once per frame
+    public void addScore(int scoreObtained)
     {
         playerScore += scoreObtained;
         updater.UpdateScoreText(playerScore);
+        //UpdateScore.Update
     }
 }
